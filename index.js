2daf96fb6a3c976b89d55ca6adf0c39cdd38d5d4
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-30 21:43:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  
 }
   BoardMemeber.prototype.veto = function() {
     return "No, I must disagree"
