ad54c2ed53fb836f6af35f0c659d70c3f431c6b5
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-30 21:47:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ function BoardMember(name, homeState, training) {
     return "You will see great things from Scuber."
   };
   
-  BoardMember.prototype.sayHi = function() {
-    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+ BoardMember.prototype.sayHi = function() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I 
+    was trained in ${this.training}.`
   };
 }
 
